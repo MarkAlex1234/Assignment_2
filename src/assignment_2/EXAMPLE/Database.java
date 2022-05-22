7df4ea2c24c3df4a2e4c9d5945631a25759c5ea6
MarkAlex1234 <mark.alexander1314@gmail.com>
2022-05-23 11:44:57 +1200

    Updated author and studentID

--- src/assignment_2/EXAMPLE/Database.java
+++ src/assignment_2/EXAMPLE/Database.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package assignment_2.EXAMPLE;
 
 import java.sql.Connection;
@@ -16,7 +11,9 @@ import java.util.logging.Logger;
 
 /**
  *
- * @author WindForce
+ * @author Mark Alexander
+ * @StudentID: 20112145
+ * 
  */
 public class Database {
 
